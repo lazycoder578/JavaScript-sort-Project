9fa4f5c5d83245d42ded38f4318dc620d5f0a0d5
Md. Abdul Kader <76205773+lazycoder578@users.noreply.github.com>
2022-07-28 00:11:52 +0600

    Add files via upload

---
+++ ACTIVE NAVBAR/index.js
@@ -0,0 +1,7 @@
+var Item=document.querySelectorAll('a');
+var CurrentLocation=location.href;
+for(var i=0;i<Item.length;i++){
+    if(Item[i].href===CurrentLocation){
+       Item[i] .className="active";
+    }
+}
