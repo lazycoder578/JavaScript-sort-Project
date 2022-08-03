33c4510ff5d84b0ccccdd46d7808710f04b3e65b
Md. Abdul Kader <76205773+lazycoder578@users.noreply.github.com>
2022-08-03 20:08:06 +0600

    Add files via upload

---
+++ SIMPLE TO-DO APP/index.js
@@ -0,0 +1,11 @@
+var add=document.getElementById("add");
+
+add.addEventListener('click',function(){
+    var OlItem=document.getElementById("OlItem");
+    var input=document.getElementById("input").value;
+
+    var text=document.createTextNode(input);
+    var NewItem=document.createElement("li");
+    NewItem.appendChild(text);
+    OlItem.appendChild(NewItem);
+});
