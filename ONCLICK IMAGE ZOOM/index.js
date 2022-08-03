49334973dec8639230893539cc511c7c846dd74b
Md. Abdul Kader <76205773+lazycoder578@users.noreply.github.com>
2022-08-03 11:42:40 +0600

    Add files via upload

---
+++ ONCLICK IMAGE ZOOM/index.js
@@ -0,0 +1,16 @@
+var main_image=document.getElementById("mainImage");
+function changeImg2(){
+    main_image.src="images/2.jpg";
+}
+function changeImg3(){
+    main_image.src="images/3.jpg";
+}
+function changeImg4(){
+    main_image.src="images/4.jpg";
+}
+function changeImg5(){
+    main_image.src="images/5.jpg";
+}
+function changeImg5(){
+    main_image.src="images/1.jpg";
+}
